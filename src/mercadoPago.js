3cfe206cc6ce4ee05c22cae1a7ba286ece207321
dcoderdesenvolvimentos-del <dcoderdesenvolvimentos@gmail.com>
2025-12-29 11:12:07 -0300

    scheduler safe execution with lock

--- src/mercadoPago.js
+++ src/mercadoPago.js
@@ -30,6 +30,7 @@ export async function createPixPayment(userPhone, planKey) {
 
       // 🔎 AÇÃO OBRIGATÓRIA DO MP (rastreio interno)
       external_reference: `user_${userPhone}_plan_${planKey}`,
+      statement_descriptor: "MARIOLEMBRA",
 
       payer: {
         email: `user${userPhone}@mariomelembra.com.br`,
