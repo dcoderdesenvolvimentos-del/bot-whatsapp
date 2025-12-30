6655f2d3bc4293cab6fe899e1590c4c785039054
dcoderdesenvolvimentos-del <dcoderdesenvolvimentos@gmail.com>
2025-12-30 13:46:46 -0300

    scheduler safe execution with lock

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import admin from "firebase-admin";
+
+const serviceAccount = JSON.parse(
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
+);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+console.log("🔥 Firebase app:", admin.apps[0]?.options?.credential?.projectId);
+
+export const db = admin.firestore();
+
+console.log("🗄️ Firestore DB:", db._settings?.databaseId);
