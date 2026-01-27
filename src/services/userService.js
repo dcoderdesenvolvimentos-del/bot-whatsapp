f674bdf6f2fe1d7a6b0d0b357a4160d2594c2ccb
dcoderdesenvolvimentos-del <dcoderdesenvolvimentos@gmail.com>
2026-01-26 21:53:34 -0300

    Testando,..

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,7 +2,7 @@ import { db } from "../config/firebase.js";
 
 export async function getOrCreateUser({ uid }) {
   if (!userId || typeof userId !== "string") {
-    throw new Error(`UID inválido recebido no getOrCreateUser: ${userId}`);
+    throw new Error(`UID inválido recebido no getOrCreateUser: ${uid}`);
   }
   const userRef = db.collection("users").doc(uid);
   const doc = await userRef.get();
@@ -19,8 +19,8 @@ export async function getOrCreateUser({ uid }) {
   return userRef;
 }
 
-export async function getUser(phone) {
-  const userRef = db.collection("users").doc(phone);
+export async function getUser(uid) {
+  const userRef = db.collection("users").doc(uid);
   const doc = await userRef.get();
 
   if (!doc.exists) {
@@ -30,13 +30,13 @@ export async function getUser(phone) {
   return doc.data();
 }
 
-export async function updateUser(phone, data) {
-  const userRef = db.collection("users").doc(phone);
+export async function updateUser(uid, data) {
+  const userRef = db.collection("users").doc(uid);
   await userRef.update(data);
 }
 
-export async function createUser(phone, data = {}) {
-  const userRef = db.collection("users").doc(phone);
+export async function createUser(uid, data = {}) {
+  const userRef = db.collection("users").doc(uid);
   await userRef.set({
     phone,
     createdAt: Date.now(),
