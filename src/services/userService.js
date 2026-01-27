ca7b6e8b1535bff29fd9f9f235fc5947c8221790
dcoderdesenvolvimentos-del <dcoderdesenvolvimentos@gmail.com>
2026-01-26 23:19:06 -0300

    Testando,..

--- src/services/userService.js
+++ src/services/userService.js
@@ -15,15 +15,3 @@ export async function updateUser(uid, data) {
   const userRef = db.collection("users").doc(uid);
   await userRef.update(data);
 }
-
-export async function createUser(uid, data = {}) {
-  const userRef = db.collection("users").doc(uid);
-  await userRef.set({
-    phone,
-    createdAt: Date.now(),
-    active: false,
-    plan: "free",
-    ...data,
-  });
-  return userRef;
-}
